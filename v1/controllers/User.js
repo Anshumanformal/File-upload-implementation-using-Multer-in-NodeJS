f6a5db9c65cb799b2d9ff33c0131230243086d02
Anshuman Ranjan <anshuman.ranjan@celebaltech.com>
2022-02-02 13:38:17 +0530

    added different storage for different use cases

--- v1/controllers/User.js
+++ v1/controllers/User.js
@@ -1,6 +1,7 @@
 
 module.exports.uploadImage = async(req, res, next) => {
     try {
+        // Here, we receive file in req.file object.
        
         if(!req.file) throw new Error("ERROR_WHILE_UPLOADING");
         console.log(req.file);
@@ -20,3 +21,43 @@ module.exports.uploadImage = async(req, res, next) => {
 }
 
 
+module.exports.uploadArrayOfImages = async(req, res, next) => {
+    try {
+       
+        // Here, we receive files in req.files object.
+
+        if(!req.files) throw new Error("ERROR_WHILE_UPLOADING");
+        // console.log('req.files=====>>>>>>>', req.files);
+
+        let newPath = [];
+
+        req.files.userImage.forEach((item) => {
+            return newPath.push(item.path.split("public").pop());
+        })
+        req.files.adminImage.forEach((item) => {
+            return newPath.push(item.path.split("public").pop());
+        })
+        req.files.superAdminImage.forEach((item) => {
+            return newPath.push(item.path.split("public").pop());
+        })
+        req.files.superAdminImage.forEach((item) => {
+            return newPath.push(item.path.split("public").pop());
+        })
+        
+        // const newPath = req.file.path.split("public").pop();
+        // const fullImageBaseURLPath = process.env.BASE_URL + newPath;
+        let fullImageBaseURLPath = [];
+        fullImageBaseURLPath = newPath.map((item) =>{
+            return process.env.BASE_URL + item;
+        })
+        
+        res.json({
+            success: true,
+            message : "IMAGE UPLOADED SUCCUSSFULLY",
+            fullImageBaseURLPath
+        })
+
+    } catch (error) {
+        next(error);
+    }
+}
--- v1/routes/User.js
+++ v1/routes/User.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
+const userstorage = multer.diskStorage({
     destination: path.resolve('public', 'uploads', 'user'),
     // destination: path.join(__dirname, "public", "uploads", "user")/* + "/public/uploads/user"*/,
     filename: function (req, file, cb) {
@@ -12,9 +12,46 @@ const storage = multer.diskStorage({
     },
 });
 
+const adminStorage = multer.diskStorage({
+    destination: path.resolve('public', 'uploads', 'admin'),
+    filename: function (req, file, cb) {
+        const extension = "".concat(file.originalname).split(".").pop();
+        const filename = Date.now().toString(36);
+        cb(null, `${filename}.${extension}`);
+    },
+});
+
+const superAdminStorage = multer.diskStorage({
+    destination: path.resolve('public', 'uploads', 'superAdmin'),
+    filename: function (req, file, cb) {
+        const extension = "".concat(file.originalname).split(".").pop();
+        const filename = Date.now().toString(36);
+        cb(null, `${filename}.${extension}`);
+    },
+});
+
+const vendorStorage = multer.diskStorage({
+    destination: path.resolve('public', 'uploads', 'vendor'),
+    filename: function (req, file, cb) {
+        const extension = "".concat(file.originalname).split(".").pop();
+        const filename = Date.now().toString(36);
+        cb(null, `${filename}.${extension}`);
+    },
+});
+
+const allStorage = multer.diskStorage({
+    destination: path.resolve('public', 'uploads', 'ImageByName'),
+    filename: function (req, file, cb) {
+        const extension = "".concat(file.originalname).split(".").pop();
+        // Either provide the image from frontend by user/admin etc... or give static name.
+        const filename = "user_" + Date.now().toString(36);
+        cb(null, `${filename}.${extension}`);
+    },
+});
+
 /*
 Note:-
-In order to separate files into folders, we need to find the user based upon token/email etc. Then apply that
+In order to separate file types(like image, document etc..) into folders, we need to find the user based upon token/email etc. Then apply that
 logic to create the filename above. Everything depends on the fieldname provided from the frontend.
 
 var assign = multer.diskStorage({
@@ -40,9 +77,28 @@ var upload = multer({storage:assign});
 
 */
 
-const upload = multer({ storage });
+
+/*
+For separating files based on filetype -> apply check on mimetype
+*/
+
+const uploadUser = multer({ storage: userstorage });
+const uploadAdmin = multer({ storage: adminStorage });
+const uploadSuperAdmin = multer({ storage: superAdminStorage });
+const uploadVendor = multer({ storage: vendorStorage });
+const uploadArrayOfImages = multer({ storage: allStorage });
 const Controller = require("../controllers");
 
-router.post("/uploadImage", upload.single("image"), Controller.UserController.uploadImage);
+router.post("/uploadImage", uploadUser.single("image"), Controller.UserController.uploadImage);
+router.post("/uploadAdminImage", uploadAdmin.single("image"), Controller.UserController.uploadImage);
+router.post("/uploadSuperAdminImage", uploadSuperAdmin.single("image"), Controller.UserController.uploadImage);
+router.post("/uploadVendorImage", uploadVendor.single("image"), Controller.UserController.uploadImage);
+
+router.post("/uploadArrayOfImages", uploadArrayOfImages.fields([
+    { name: 'userImage', maxCount: 1 },
+    { name: 'adminImage', maxCount: 1 },
+    { name: 'superAdminImage', maxCount: 1 },
+    { name: 'vendorImage', maxCount: 1 }
+]), Controller.UserController.uploadArrayOfImages);
 
 module.exports = router;
